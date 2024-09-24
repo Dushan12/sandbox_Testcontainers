1098be929d4c990b89de461e274d6eb892043c75
Dushan12 <gajikdushan@gmail.com>
2024-09-24 11:56:26 +0200

    Try to create reliable sending message algorythm with locks and exactly once guarantee

--- TestContainers/src/main/scala/actors/MailQueueActor.scala
+++ TestContainers/src/main/scala/actors/MailQueueActor.scala
@@ -6,9 +6,26 @@ import org.apache.pekko.actor.typed.scaladsl.{ActorContext, Behaviors}
 import repository.PersonRepository
 import services.RabbitMqService
 import zio.{Schedule, Unsafe, ZIO, ZLayer}
-
 import java.time.temporal.ChronoUnit
 
+/***
+ * Why use queue when we can send them and reload them
+ * Why not use database
+ *
+ * If you see this from a perspective where the service is hosted on distributed environment
+ * even if the service is singleton in the application it has multiple services like this on every instance of the
+ * cluster. So if you have five instances all of them will try to pull from database and send the message
+ * and this causes issue if you want the system to send the mail only once.
+ *
+ * Queue have a deduplication mechanisms that if a message is seen on the exchange it will be ignored in the next occurance
+ * Redis has mechanism where when you store the value you can specify NX=true which means only once instance will get to save
+ * the value and then all others will get the cached value which is also an option.
+ * In that case the app will try to store a value and if it succeeds then send the mail
+ * if the app do not succeed means another instance already did and fail gracefully or get the result from Reddis
+ * https://redis.io/docs/latest/develop/use/patterns/distributed-locks/
+
+
+ */
 
 object MailQueueActor {
   final case class QueuePendingMails()
